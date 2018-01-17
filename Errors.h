683c86eca0d1fa7c53cb5c687bd0e6610ad2feaf
Yan <34117137+ychacarr@users.noreply.github.com>
2018-01-17 10:52:54 +0600

    Delete Errors.h

--- Errors.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef H_ERRORFUNC
-#define H_ERRORFUNC
-
-namespace ERROR {
-	void throwError(const char *EMSG, unsigned int inID = 0);
-	void throwError(const std::string &EMSG, unsigned int inID = 0);
-}
-#endif // !H_ERRORFUNC
